22f5e6e5c25f86b269ffe20db14fe893cebd5af6
alexmcgreevy <119376428+alexmcgreevy@users.noreply.github.com>
2026-03-12 22:45:31 +0900

    Add Mad-Libs game template in project_a_madlibs.py
    Initial commit of the Mad-Libs game template.

---
+++ project_a_madlibs.py
@@ -0,0 +1,11 @@
+# PROJECT A: MAD-LIBS
+# In this game, you ask the user for parts of speech (nouns, verbs, etc.)
+# and then plug them into a story template.
+
+# 1. Create at least 5 variables that take input from the user.
+# Example: name = input("Enter a name: ")
+
+# 2. Print out your story using these variables.
+# HINT: print("Once upon a time, " + name + " went to the " + place + "...")
+
+# WRITE YOUR CODE BELOW:
