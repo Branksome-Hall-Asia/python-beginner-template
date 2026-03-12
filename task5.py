fdb918a5de9d688b9a8e1a6751b7f2b0ac079706
alexmcgreevy <119376428+alexmcgreevy@users.noreply.github.com>
2026-03-12 22:42:02 +0900

    Create task5.py with scoring instructions
    Add task 5 instructions for a simple scoring program.

---
+++ task5.py
@@ -0,0 +1,10 @@
+# TASK 5: THE BOUNCER
+# The 'if' statement lets the computer make decisions.
+# 1. Create a variable called 'user_score' and set it to 85.
+# 2. Write an IF statement: 
+#    - If the score is 80 or more, print "You passed!"
+#    - Else, print "Try again!"
+
+# HINT: Don't forget the colon (:) and the indentation (tab space)!
+
+# WRITE YOUR CODE BELOW:
